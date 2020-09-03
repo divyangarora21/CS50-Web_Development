abf01e284285695423a5240de541fceea02b1b2d
divyangarora21 <divyangarora2104@gmail.com>
2020-09-03 17:27:38 +0530

    updated the files

--- lecture_2/sequences.py
+++ lecture_2/sequences.py
@@ -1,3 +1,10 @@
 name = "Alice"
+
+#Creating a tupple
 coordinates = (10.0, 20.0)
+
+#creating a list 
 names = ["Alice", "Bob", "Charlie"]
+
+#printing the value of the tupple
+print(coordinates)
--- lecture_2/sets.py
+++ lecture_2/sets.py
@@ -5,9 +5,13 @@ s = set()
 s.add(1)
 s.add(3)
 s.add(5)
+s.add(4)
 s.add(3)
 print(s)
 
 # Remove elemnt from set
 s.remove(5)
 print(s)
+
+#printing size of the set 
+print(f"The number of elements in the set are {len(s)}")
