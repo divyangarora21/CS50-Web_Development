3960df4dd0604cd00cac678baf118a15f20c6e0a
divyangarora21 <divyangarora2104@gmail.com>
2020-09-04 12:17:58 +0530

    4 sept

---
+++ lecture_3/basic/hello/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+	return HttpResponse("Hello World!")	
+def greet(request,name):
+	return HttpResponse("Hello,{name}".format(name))
